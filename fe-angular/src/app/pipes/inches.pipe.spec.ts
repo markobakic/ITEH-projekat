119bc320df92f5d812b80b88f28a5faf2b25fd2a
Jovana Cutovic <jovana.cutovic@gmail.com>
2022-06-23 21:22:04 +0200

    Kreiranje pipe-ova

---
+++ fe-angular/src/app/pipes/inches.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { InchesPipe } from './inches.pipe';
+
+describe('InchesPipe', () => {
+  it('create an instance', () => {
+    const pipe = new InchesPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
